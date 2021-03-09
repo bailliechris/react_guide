d2442ec6c38b8685cc9d3668044d58d053a2b1b0
bailliechris <bailliechristopher@gmail.com>
2021-03-09 21:31:56 +0000

    Added fetch elements

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
     getTasks()
   }, [])
 
+  // Get data from server
   const fetchTasks = async () => {
     const res = await fetch('http://localhost:5000/tasks')
     const data = await res.json()
@@ -22,24 +23,44 @@ function App() {
   }
 
   // Delete Task
-  const deleteTask = (id) => {
-    setTasks(tasks.filter((task) => task.id !== id))
+  const deleteTask = async (id) => {
+    const res = await fetch('http://localhost:5000/tasks/${id}',
+      {
+        method: 'DELETE'
+      })
+    
+    res.status = 200
+      ? setTasks(tasks.filter((task) => task.id !== id))
+      : alert('Error Deleting this Task')
   }
 
   //Toggle Reminder
   const toggleReminder = (id) => {
+    // Go through all tasks (map) and if statement shorthand to swap value else leave it alone
     setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
   }
 
-  const addTask = (task) => {
+  // Add Task
+  const addTask = async (task) => {
     // Add random ID
-    const id = Math.floor(Math.random() * 10000) + 1
+/*    const id = Math.floor(Math.random() * 10000) + 1
 
     // Create new task object, adding id and filling with remainder of the task
     const newTask = { id, ...task }
 
     // Use State, add existing tasks and include new task at the end
-    setTasks([...tasks, newTask])
+    setTasks([...tasks, newTask]) */
+    const res = await fetch('http://localhost:5000/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(task)
+    })
+
+    const data = res.json()
+
+    setTasks([...tasks, data])
   }
 
   return (
